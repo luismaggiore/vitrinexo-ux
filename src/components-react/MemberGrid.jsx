4c5f1c0a2e14115febb8cba4ead6c117fb6249a8
luissalazarfigueroa <luis.salazar.f94@gmail.com>
2026-05-09 16:23:03 -0400

    avanzando maqueta

--- src/components-react/MemberGrid.jsx
+++ src/components-react/MemberGrid.jsx
@@ -3,7 +3,7 @@ import { members } from "../data/members";
 
 export default function MemberGrid() {
   return (
-    <div className="row g-3">
+    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 row-cols-xl-5 g-2">
       {members.map((member) => (
         <MemberCard key={member.name} {...member} />
       ))}
